6b91ce6c87baacbe34cb0e221aa9ab12c12d7ee6
nupamore <nupamore@gmail.com>
2020-11-12 14:09:51 +0900

    cleanup

--- src/api/API.js
+++
@@ -1,7 +0,0 @@
-export default {
-    LOGOUT: ['GET', '/auth/logout'],
-    GET_PROFILE: ['GET', '/api/v1/me'],
-    GET_UPLOADERS: ['GET', '/api/v1/guilds/{guildId}/uploaders'],
-    GET_IMAGES: ['GET', '/api/v1/guilds/{guildId}/images'],
-    DELETE_IMAGE: ['DELETE', '/api/v1/images/{imageId}'],
-}
--- src/plugins/element-ui.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue'
-
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
-
-export default ({ app }) =>
-    Vue.use(ElementUI, {
-        i18n: (key, val) => app.i18n.t(key, val),
-    })
